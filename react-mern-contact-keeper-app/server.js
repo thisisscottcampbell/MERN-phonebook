a635f9f9f16881ed688a5bdc2826b1ceb8abcd89
Scott Campbell <stc9486@gmail.com>
2020-04-17 10:18:22 -0700

    server running

---
+++ react-mern-contact-keeper-app/server.js
@@ -0,0 +1,8 @@
+//'import' express
+const express = require('express')
+//initialze express
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
