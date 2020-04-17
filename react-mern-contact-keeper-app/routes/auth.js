7b5baa741ea264bcaac1301d5ee2f6c6f4d134f0
Scott Campbell <stc9486@gmail.com>
2020-04-17 10:58:41 -0700

    auth

---
+++ react-mern-contact-keeper-app/routes/auth.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+//@route    GET api/auth
+//@desc     Get a logged in user
+//@access   Private 
+
+router.get('/', (req, res) => {
+  res.send('Get logged in a user');
+});
+
+//@route    POST api/auth
+//@desc     Auth user & get token
+//@access   Public
+
+router.get('/', (req, res) => {
+  res.send('Log in a user');
+});
+
+module.exports = router
