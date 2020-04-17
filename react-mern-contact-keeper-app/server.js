dca9fcab96c8c17cf1a8cb3864a6e1ce227bb445
Scott Campbell <stc9486@gmail.com>
2020-04-17 10:24:28 -0700

    get

--- react-mern-contact-keeper-app/server.js
+++ react-mern-contact-keeper-app/server.js
@@ -1,8 +1,12 @@
 //'import' express
 const express = require('express')
 //initialze express
-const app = express();
-const PORT = process.env.PORT || 5000
+const app = express()
 
+app.get('/', (req, res) => res.json({msg: 'Welcome to the contact keeper api'}));
+
+
+
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
