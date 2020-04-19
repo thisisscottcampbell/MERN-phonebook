493ac43fb5e05ab4a6595a0e56c47ca4bf00612d
Scott Campbell <stc9486@gmail.com>
2020-04-18 19:27:51 -0700

    a/a

--- react-mern-contact-keeper-app/config/db.js
+++ react-mern-contact-keeper-app/config/db.js
@@ -2,7 +2,30 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = () => {
+const connectDB = async () => {
+  try {
+    await mongoose
+    .connect(db, {
+      useNewUrlParser: true, 
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+
+    console.log('MongoDB Connected...')
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
+
+
+//For comparison
+
+/*
+
+const conectDB = () => {
   mongoose
   .connect(db, {
     useNewUrlParser: true,
@@ -16,4 +39,4 @@ const connectDB = () => {
   })
 }
 
-module.exports = connectDB
+*/
