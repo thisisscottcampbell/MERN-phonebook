ab1d4a15af0e8117e4bcf5329aa109d776420a07
Scott Campbell <stc9486@gmail.com>
2020-04-18 19:38:03 -0700

    middle

--- react-mern-contact-keeper-app/server.js
+++ react-mern-contact-keeper-app/server.js
@@ -15,7 +15,6 @@ app.use(express.json({ extended: false }))
 app.get('/', (req, res) => res.json({msg: 'Welcome to the contact keeper api'}));
 
 //Define Routes
-
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
