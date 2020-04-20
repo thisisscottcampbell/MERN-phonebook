c34464515c8faae347b06e5a074c01c48e0680cf
Scott Campbell <stc9486@gmail.com>
2020-04-19 21:41:03 -0700

    get/auth

--- react-mern-contact-keeper-app/middleware/auth.js
+++ react-mern-contact-keeper-app/middleware/auth.js
@@ -11,7 +11,8 @@ module.exports = function(req, res, next) {
   try {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
     req.user = decoded.user;
+    next();
   } catch (err) {
-
+    res.status(401).json({ msg: 'Token is not valid' })
   }
 }
