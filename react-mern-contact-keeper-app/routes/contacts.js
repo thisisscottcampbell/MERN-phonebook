ec175df089ee02200e9ff293dfac39d5e931c6f3
Scott Campbell <stc9486@gmail.com>
2020-04-22 13:09:32 -0700

    delete

--- react-mern-contact-keeper-app/routes/contacts.js
+++ react-mern-contact-keeper-app/routes/contacts.js
@@ -94,8 +94,26 @@ router.put('/:id', auth, async (req, res) => {
 //@desc     Delete a contact
 //@access   Private 
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',auth, async (req, res) => {
   res.send('Delete a contact');
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if(!contact) return res.status(404).json({ msg: 'Contact not found' })
+    
+    //make sure user owns contact
+    if(contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' })
+
+    }
+    
+    await Contact.findByIdAndRemove(req.params.id);
+
+    res.json({msg: "Contact removed"});
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
 });
 
 
