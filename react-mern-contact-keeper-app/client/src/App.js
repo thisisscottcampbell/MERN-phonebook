b683b1f0ae900a0c0558500cc587f07a44a60c29
Scott Campbell <stc9486@gmail.com>
2020-04-28 19:22:17 -0700

    contact state

--- react-mern-contact-keeper-app/client/src/App.js
+++ react-mern-contact-keeper-app/client/src/App.js
@@ -5,20 +5,23 @@ import Navbar from '../src/components/layout/Navbar'
 import './App.css';
 import Home from '../src/components/pages/Home'
 import About from '../src/components/pages/About'
+import ContactState from '../context/contact/ContactState'
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar /> 
-        <div className="container">
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </div> 
-      </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar /> 
+          <div className="container">
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div> 
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 };
 
