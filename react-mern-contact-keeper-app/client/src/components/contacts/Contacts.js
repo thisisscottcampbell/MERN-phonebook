e653e09f49e3251bc8ef9625563e116823eb2676
Scott Campbell <stc9486@gmail.com>
2020-05-01 09:59:36 -0700

    comps

---
+++ react-mern-contact-keeper-app/client/src/components/contacts/Contacts.js
@@ -0,0 +1,10 @@
+import React, { Fragment, useContext } from 'react'
+import ContactContext from '../../context/contact;/ContactContext'
+
+export const Contacts = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
