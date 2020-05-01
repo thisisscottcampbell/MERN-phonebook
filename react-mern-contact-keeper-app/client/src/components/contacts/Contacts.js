f2466e10b3d228de2195b1a2a9c823f1c2139688
Scott Campbell <stc9486@gmail.com>
2020-05-01 10:09:08 -0700

    contacts

--- react-mern-contact-keeper-app/client/src/components/contacts/Contacts.js
+++ react-mern-contact-keeper-app/client/src/components/contacts/Contacts.js
@@ -2,9 +2,13 @@ import React, { Fragment, useContext } from 'react'
 import ContactContext from '../../context/contact;/ContactContext'
 
 export const Contacts = () => {
+  const contactContext = useContext(ContactContext)
+  const { contacts } = ContactContext
   return (
-    <div>
-      
-    </div>
+    <Fragment>
+      {contacts.map(contact => (
+        <h3>{contact.name}</h3>
+      ))}
+    </Fragment>
   )
 }
