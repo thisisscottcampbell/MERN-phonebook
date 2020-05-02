eb51eb23e1700ea9282ab6668cb651555fb4e0fc
Scott Campbell <stc9486@gmail.com>
2020-05-02 11:05:06 -0700

    email, phone, btn

--- react-mern-contact-keeper-app/client/src/components/contacts/ContactItem.js
+++ react-mern-contact-keeper-app/client/src/components/contacts/ContactItem.js
@@ -7,7 +7,8 @@ const ContactItem = ({ contact }) => {
     <div className="card bg-light">
       <h3 className="text-primary text-left">
         {name}{' '} 
-        <span 
+        <span
+        style={{float: 'right'}} 
         className={
           'badge ' + 
           (type === 'professional' ? 'badge-success' : 'badge-primary')
@@ -16,6 +17,18 @@ const ContactItem = ({ contact }) => {
         {type.charAt(0).toUpperCase() + type.slice(1)}
       </span>
       </h3>
+      <ul className="list">
+        {email && (<li>
+          <i className="fas fa-envelope-open"></i> {email}
+        </li>)}
+        {phone && (<li>
+          <i className="fas fa-phone"></i> {phone}
+        </li>)}
+      </ul>
+      <p>
+        <button className="btn btn-dark btn-sm">Edit</button>
+        <button className="btn btn-dark btn-sm">Delete</button>
+      </p>
     </div>
   )
 }
