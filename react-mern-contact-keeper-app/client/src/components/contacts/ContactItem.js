6d9067265d5b8f9bb06c0f60b72306c5bf9391a2
Scott Campbell <stc9486@gmail.com>
2020-05-02 10:59:35 -0700

    upper

--- react-mern-contact-keeper-app/client/src/components/contacts/ContactItem.js
+++ react-mern-contact-keeper-app/client/src/components/contacts/ContactItem.js
@@ -6,7 +6,15 @@ const ContactItem = ({ contact }) => {
   return (
     <div className="card bg-light">
       <h3 className="text-primary text-left">
-        {name}{' '} <span className={'badge ' + (type === 'professional' ? 'badge-success' : 'badge-primary')}>{type}</span>
+        {name}{' '} 
+        <span 
+        className={
+          'badge ' + 
+          (type === 'professional' ? 'badge-success' : 'badge-primary')
+        }
+      >
+        {type.charAt(0).toUpperCase() + type.slice(1)}
+      </span>
       </h3>
     </div>
   )
