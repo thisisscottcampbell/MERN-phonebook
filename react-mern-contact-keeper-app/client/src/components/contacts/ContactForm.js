372c277c49e70c42fb5370bdcca122870cdfbe06
Scott Campbell <stc9486@gmail.com>
2020-05-02 11:36:41 -0700

    radios

--- react-mern-contact-keeper-app/client/src/components/contacts/ContactForm.js
+++ react-mern-contact-keeper-app/client/src/components/contacts/ContactForm.js
@@ -16,32 +16,38 @@ const ContactForm = () => {
       <h2  className='text-primary'>Add Contact</h2>
       <input 
         type='text'
-        placeholder='name'
+        placeholder='Name'
         name='name'
         value={name}
         onChange={onChange}
       />
          <input 
         type='text'
-        placeholder='email'
+        placeholder='Email'
         name='email'
         value={email}
         onChange={onChange}
       />
          <input 
         type='text'
-        placeholder='phone'
+        placeholder='Phone'
         name='phone'
         value={phone}
         onChange={onChange}
       />
-         <input 
-        type='text'
-        placeholder='type'
-        name='type'
-        value={type}
-        onChange={onChange}
-      />
+      <h5>Contact Type</h5>
+        <input 
+          type='radio'
+          name='type'
+          value='personal'
+          checked={type === 'personal'}
+        /> Personal{' '}
+        <input 
+          type='radio'
+          name='type'
+          value='personal'
+          checked={type === 'professioanl'}
+        /> Professional{' '}
     </div>
   )
 }
