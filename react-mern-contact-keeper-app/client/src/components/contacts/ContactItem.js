d4006ce0b3c81d04fb0d5b773882672211d4bef0
Scott Campbell <stc9486@gmail.com>
2020-05-18 12:28:36 -0400

    edit context

--- react-mern-contact-keeper-app/client/src/components/contacts/ContactItem.js
+++ react-mern-contact-keeper-app/client/src/components/contacts/ContactItem.js
@@ -3,7 +3,7 @@ import ContactContext from '../../context/contact/contactContext'
 
 const ContactItem = ({ contact }) => {
   const contactContext = useContext(ContactContext);
-  const { deleteContact } = contactContext;
+  const { deleteContact, setCurrent } = contactContext;
 
   const { id, name, email, phone, type } = contact;
 
@@ -38,7 +38,7 @@ const ContactItem = ({ contact }) => {
         )}
       </ul>
       <p>
-        <button className="btn btn-dark btn-sm">Edit</button>
+        <button className="btn btn-dark btn-sm" onClick={() => setCurrent(contact)}>Edit</button>
         <button className="btn btn-danger btn-sm"onClick={onDelete}>Delete</button>
       </p>
     </div>
