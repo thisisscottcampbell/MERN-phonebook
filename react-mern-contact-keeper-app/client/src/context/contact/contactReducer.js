7b540a49f7bb0c6283af3f45bd406444d619e38e
Scott Campbell <stc9486@gmail.com>
2020-05-18 12:25:55 -0400

    reducer clear

--- react-mern-contact-keeper-app/client/src/context/contact/contactReducer.js
+++ react-mern-contact-keeper-app/client/src/context/contact/contactReducer.js
@@ -2,8 +2,7 @@ import {
   ADD_CONTACT,
   DELETE_CONTACT,
   SET_CURRENT,
-  // SET_CURRENT,
-  // CLEAR_CURRENT,
+  CLEAR_CURRENT,
   // UPDATE_CONTACT,
   // FILTER_CONTACTS,
   // CLEAR_FILTER
@@ -25,6 +24,11 @@ export default (state, action) => {
       return {
         ...state,
         current: action.payload
+      }
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null
       }
     default:
       return state;
